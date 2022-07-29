4d573fd99e39cb56310a6db3341c3dc679a63ffc
Shayan <snikfar@ucsd.edu>
2022-07-28 22:52:03 -0700

    vsdvs

--- blog.js
+++ blog.js
@@ -26,22 +26,21 @@ function newpost(){
         id: '',
      };
 
-    newpostarr.title = cleanHTML`${document.getElementById('title').value}`;
-    newpostarr.date = cleanHTML`${document.getElementById('date').value}`;
-    newpostarr.summary = cleanHTML`${document.getElementById('summary').value}`;
+    newpostarr.title = cleanHTML`${document.getElementById('titlepost').value}`;
+    newpostarr.date = cleanHTML`${document.getElementById('datepost').value}`;
+    newpostarr.summary = cleanHTML`${document.getElementById('summarypost').value}`;
     newpostarr.id=numpost++;
 
-    let arr =[];
-    arr = Arrposts;
-    arr.push(newpost);
+    let arr = Arrposts;
+    arr.push(newpostarr);
 
     htmlwritten();
-    document.getElementById("title").value = '';
+    document.getElementById("titlpost").value = '';
 
-    document.getElementById("date").value = '';
+    document.getElementById("datepost").value = '';
 
-    document.getElementById("summary").value = '';
-    document.getElementById("newpost").close();
+    document.getElementById("summarypost").value = '';
+    document.getElementById("dialogpost").close();
 
 
 }
@@ -59,38 +58,38 @@ function updatePost(id) {
 
 
 
-    upPost.title = cleanHTML`${document.getElementById("edtitle").value}`;
-    upPost.date = cleanHTML`${document.getElementById("eddate").value}`;
-    upPost.summary = cleanHTML`${document.getElementById("edsummary").value}`;
+    upPost.title = cleanHTML`${document.getElementById("titleupdate").value}`;
+    upPost.date = cleanHTML`${document.getElementById("dateupdate").value}`;
+    upPost.summary = cleanHTML`${document.getElementById("summaryupdate").value}`;
     upPost.id=id;
     console.log(id);
     let uparr = Arrposts;
     uparr.push(upPost);
     htmlwritten();
-    document.getElementById("edtitle").value = '';
-    document.getElementById("eddate").value = '';
-    document.getElementById("edsummary").value = '';
-    document.getElementById("editpost").close();
+    document.getElementById("titleupdate").value = '';
+    document.getElementById("dateupdate").value = '';
+    document.getElementById("summaryupdate").value = '';
+    document.getElementById("dialogupdate").close();
 
 }
 
-let mytemp = document.getElementById("posted");
+let mytemp = document.getElementById("postedtemp");
 var uptoU;
 function postHtml(post, mytemp){
     let pclon = mytemp.content.cloneNode(true);
 
-    pclon.getElementById("pname").textContent=cleanHTML`${post.title}`;
-    pclon.getElementById("pdate").textContent=cleanHTML`${post.date}`;
-    pclon.getElementById("psummary").textContent=cleanHTML`${post.summary}`;
+    pclon.getElementById("titlepost").textContent=cleanHTML`${post.title}`;
+    pclon.getElementById("datepost").textContent=cleanHTML`${post.date}`;
+    pclon.getElementById("summarypost").textContent=cleanHTML`${post.summary}`;
 
     
     pclon.getElementById("edited").addEventListener('click', () => {
 
-      document.getElementById("edtitle").value = Arrposts.filter(item => item.id === post.id)[0].title;
-      document.getElementById("eddate").value = Arrposts.filter(item => item.id === post.id)[1].date;
-      document.getElementById("edsummary").value = Arrposts.filter(item => item.id === post.id)[2].summary;
+      document.getElementById("titleupdate").value = Arrposts.filter(item => item.id === post.id)[0].title;
+      document.getElementById("dateupdate").value = Arrposts.filter(item => item.id === post.id)[0].date;
+      document.getElementById("summaryupdate").value = Arrposts.filter(item => item.id === post.id)[0].summary;
       uptoU = post.id;
-      document.getElementById("editdialog").showModal();
+      document.getElementById("dialogupdate").showModal();
     });
   
     pclon.getElementById("deleted").addEventListener('click', () => {
@@ -123,22 +122,22 @@ document.addEventListener('DOMContentLoaded', () => {
     htmlwritten(Arrposts);
   
     document.getElementById("postcreated").addEventListener('click', () => {
-      if (typeof document.getElementById("newpost").showModal === "function") {
-        document.getElementById("newpost").showModal();
+      if (typeof document.getElementById("dialogpost").showModal === "function") {
+        document.getElementById("dialogpost").showModal();
       } 
       else {
         alert("not supported");
       }
     });
   
-    document.getElementById("newpost").addEventListener('close', () => {
-      if (document.getElementById("newpost").returnValue === 'default') {
+    document.getElementById("dialogpost").addEventListener('close', () => {
+      if (document.getElementById("dialogpost").returnValue === 'default') {
         newpost();
       }
     });
   
-    document.getElementById("editpost").addEventListener('close', () => {
-      if (document.getElementById("editpost").returnValue === 'default') {
+    document.getElementById("dialogupdate").addEventListener('close', () => {
+      if (document.getElementById("dialogupdate").returnValue === 'default') {
         updatePost(uptoU);
       }
     });
